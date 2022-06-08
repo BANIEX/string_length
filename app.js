89aa05777f77301b72a73a9841fba25d453ad512
baniex <banjibaniex1@gmail.com>
2022-06-08 15:15:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName("calculate")[0].addEventListener("click", function(){
+    let userInput = document.getElementById("data").value;
+    let userInputLength = userInput.length;
+    document.getElementsByClassName("answer")[0].innerHTML = userInputLength
+})
